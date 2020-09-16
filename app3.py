7310be98ba36544dc6ca85f4768559dcd5e0cca1
Ivan Luhovskyi <wotlegionb@gmail.com>
2020-09-16 22:57:40 +0300

    home work 3

---
+++ app3.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from faker import Faker
+from utils import exec_query
+#import pdb; pdb.set_trace()
+import requests
+import sqlite3
+
+app3 = Flask(__name__)
+
+
+@app3.route('/tracks-sec/')
+def names():
+    import sqlite3
+    conn = sqlite3.connect('./chinook.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT (Name), (Milliseconds) FROM tracks;')
+    res = cursor.fetchall()
+    conn.close()
+    return str(res)
+
+
+
+
+#    return str(exec_query('SELECT * FROM customers;'))
+
+if __name__ == "__main__":
+    app3.run()
+
