737a4555416de36e3ea4b0252161d86d728bc395
Ivan Luhovskyi <wotlegionb@gmail.com>
2020-09-16 23:09:16 +0300

    home work 3

--- app1.py
+++ app1.py
@@ -1,19 +1,28 @@
 from flask import Flask
 from faker import Faker
+from utils import exec_query
+#import pdb; pdb.set_trace()
+import requests
+import sqlite3
 
 app1 = Flask(__name__)
 
 
-@app1.route('/home/')
-def generate():
-    f = Faker()
-    data = ''
-    for _ in range(100):
-        data += f.email() + f.name()
-    return data
-    print('\n'.join(data))
+@app1.route('/names/')
+def names():
+    import sqlite3
+    conn = sqlite3.connect('./chinook.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT COUNT (DISTINCT FirstName) FROM customers;')
+    res = cursor.fetchall()
+    conn.close()
+    return str(res)
+
+
+
+
+#    return str(exec_query('SELECT * FROM customers;'))
 
 if __name__ == "__main__":
     app1.run()
-
 
